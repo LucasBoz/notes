18c0f7c459d3d2047fb8bccc758059ac5dfa85fb
LucasBoz <lucas_boz@hotmail.com>
2015-10-27 16:18:22 -0200

    Ajuste na listagem de notas

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -10,6 +10,10 @@
     function MainController($timeout, webDevTec, toastr, $mdToast, $scope) {
         var vm = this;
 
+        vm.newItem = {
+            "done": false,
+            "value": ""
+        };
         vm.awesomeThings = [];
         vm.classAnimation = '';
         vm.creationDate = 1445864494819;
@@ -21,27 +25,42 @@
 
         vm.note = $scope.note;
 
-        vm.note.item.push("");
 
+        vm.removeEmpty = function (index) {
+            if (vm.isEmpty(vm.note.item[index])) {
+                vm.note.item.splice(index, 1);
+            }
+        };
 
+        vm.isEmpty = function (item) {
+            if (item.value == vm.newItem.value) {
+                return true;
+            } else if (item.done == vm.newItem.done) {
+                return true;
+            }
+            return false;
+        };
 
         $scope.$watchCollection('note.item', function (newValue, oldValue) {
-            if (!!newValue && !!newValue[newValue.length -1 ]) {
+            if (vm.note.item.length == 0) {
                 vm.note.item.push("");
-                console.log('Name changed to ' + newValue);
             }
 
-            for (var i = 0; i < vm.note.item.length - 1; i++) {
-                if(vm.note.item[i] == ""){
-                    vm.note.item.splice(i,1) ;
+            if ((vm.note.item.length >= 1) &&
+                (newValue[newValue.length - 1].value != "" ) &&
+                (!!newValue[newValue.length - 1].value) ) {
+
+                if (!!newValue[newValue.length - 1]) {
+                    vm.note.item.push("");
                 }
-
             }
+
+
         });
 
 
-        vm.save = function save(){
-            if(vm.note) {
+        vm.save = function save() {
+            if (vm.note) {
                 localStorage.setItem('note', JSON.stringify(vm.note));
                 $mdToast.show(
                     $mdToast.simple()
@@ -52,7 +71,7 @@
             }
         };
 
-        vm.getNotes = function(){
+        vm.getNotes = function () {
             vm.notes = JSON.parse(localStorage.getItem('note'));
         };
         vm.getNotes();
@@ -64,6 +83,7 @@
                 vm.classAnimation = 'rubberBand';
             }, 4000);
         }
+
         activate();
 
         function showToastr() {
