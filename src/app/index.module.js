da812cd8c58e282cea09926de3df80ba8f1d6865
Lucas Boz <lucas_boz@hotmail.com>
2016-09-20 17:55:04 -0300

    Init gh-pages

--- src/app/index.module.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  'use strict';
-
-  angular
-    .module('notes', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngRoute', 'ngMaterial', 'toastr']);
-
-})();
